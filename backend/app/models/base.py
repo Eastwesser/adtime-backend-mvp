b74ccc4b5700b0892a7313e83bf7bd2636d843b8
Денис Матвеев <alttert@mail.ru>
2025-08-02 19:59:17 +0300

    baseclass model update

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,3 +1,9 @@
+"""
+Базовый класс для всех моделей SQLAlchemy.
+
+Создает declarative_base() - основу для объявления моделей.
+Все модели должны наследоваться от этого класса.
+"""
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
