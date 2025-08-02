fdec34753b8cd0e2f138b191e1d5592e6fdfa665
Денис Матвеев <alttert@mail.ru>
2025-08-02 19:57:58 +0300

    models init update, added comments

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,13 @@
-# Реэкспорт всех SQLAlchemy моделей для удобного импорта
+"""
+Экспорт всех моделей SQLAlchemy для удобного импорта из других модулей.
+
+Содержит:
+- Generation (как GenerationTask для обратной совместимости)
+- MarketItem, Factory - модели маркетплейса
+- Order - модель заказов
+- Subscription - модель подписок
+- User - модель пользователей
+"""
 from .generation import Generation as GenerationTask
 from .marketplace import MarketItem, Factory
 from .order import Order
